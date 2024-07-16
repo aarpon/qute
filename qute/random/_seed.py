ebf6a31ee5096c735283e47dca0b45a27b4a1b12
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2024-07-16 23:55:51 +0200

    Only initialize the CUDA random number generator seeds if CUDA is available.

--- qute/random/_seed.py
+++ qute/random/_seed.py
@@ -35,7 +35,8 @@ def set_global_rng_seed(seed: Union[int, None], workers: bool = False):
 
     # Explicitly set the seed for PyTorch (though it's already handled by monai.utils.set_determinism)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
     # Explicitly set the seed for PyTorch Lightning
     pl.seed_everything(seed, workers=workers)
