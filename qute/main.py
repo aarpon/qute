28fa77d7ad6ed8022063feef7bb42192377fa53d
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2024-11-25 18:08:04 +0100

    Add detailed version information to `qute version`.

--- qute/main.py
+++ qute/main.py
@@ -10,6 +10,7 @@
 #  ******************************************************************************
 import multiprocessing as mp
 import sys
+from typing import Optional
 
 import typer
 
@@ -20,9 +21,38 @@ app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
-def version():
+def version(
+    detailed: Optional[bool] = typer.Option(
+        False,
+        "-d",
+        "--detailed",
+        help="Show additional version information.",
+        show_default=False,
+    )
+):
     """Print version information."""
-    typer.echo(__version__)
+    if detailed:
+        """Display detailed version information."""
+        import subprocess
+
+        import pytorch_lightning as pl
+        import torch
+        import torchvision
+
+        typer.echo(f"qute {__version__}")
+        typer.echo(
+            f"PyTorch {torch.__version__} with torchvision {torchvision.__version__}"
+        )
+        typer.echo(f"PyTorch-Lightning {pl.__version__}")
+        try:
+            cmd = ["nvidia-smi", "--version"]
+            result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = result.stdout.decode("utf-8")
+            typer.echo(output)
+        except FileNotFoundError:
+            pass
+    else:
+        typer.echo(__version__)
 
 
 # Add sub-commands
