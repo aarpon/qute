25fc9d7f125535acd471288da1a6ce68db648ef1
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2025-01-22 18:30:21 +0100

    Update copyright note.

--- qute/transforms/_transforms.py
+++ qute/transforms/_transforms.py
@@ -1,5 +1,5 @@
 # ******************************************************************************
-# Copyright © 2022 - 2024, ETH Zurich, D-BSSE, Aaron Ponti
+# Copyright © 2022 - 2025, ETH Zurich, D-BSSE, Aaron Ponti
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License Version 2.0
 # which accompanies this distribution, and is available at
